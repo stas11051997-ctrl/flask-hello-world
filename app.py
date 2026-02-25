464a837e8fea0041d441450009848b01019da4a5
stas11051997-ctrl <stas11051997@gmail.com>
2026-02-25 21:36:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,4 @@ SubID: {subid}
     return "OK"
     
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
